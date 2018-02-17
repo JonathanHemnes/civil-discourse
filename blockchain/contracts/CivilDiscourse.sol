cd550737eed06da71ed376a87f89a94255cf6c79
Jonathan Hemnes <jonathan.hemnes@talentkode.com>
2018-02-17 15:25:42 -0700

    added id to statement

--- blockchain/contracts/CivilDiscourse.sol
+++ blockchain/contracts/CivilDiscourse.sol
@@ -4,18 +4,22 @@ contract CivilDiscourse {
     struct Statement {
         string text;
         address owner;
+        uint id;
     }
 
     event StatementAdded(address by, string text);
 
     Statement[] public statements;   
 
-    function AddStatement(string text) public {
-       statements.push(Statement({
+    function AddStatement(string text) public returns (uint){
+        var stmt = Statement({
            owner: msg.sender,
-           text: text
-       }));
+           text: text,
+           id: statements.length + 1
+       })
+       statements.push(stmt);
        StatementAdded(msg.sender, text);
+       return stmt.id;
     }
 
     function GetStatements() public view returns (Statement[]) {
